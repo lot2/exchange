d865a0aa9f00f5dbd27bed4e99c27265ba3e4131
lot.xu <lot.xu@apeksys.com>
2014-11-11 17:38:48 +0800

    20141111 pause the project of django

--- online/views.py
+++ online/views.py
@@ -32,7 +32,7 @@ def login(req):
     if req.method == 'POST':
         uf = UserForm(req.POST)
         if uf.is_valid():
-            #获取表单用户密码
+            #获取表单用户密码1
             username = uf.cleaned_data['username']
             password = uf.cleaned_data['password']
             #获取的表单数据与数据库进行比较
@@ -45,7 +45,7 @@ def login(req):
                 return response
             else:
                 #比较失败，还在login
-                return HttpResponseRedirect('/online/login/')
+                return HttpResponseRedirect("/online/login/")
     else:
         uf = UserForm()
     return render_to_response('login.html',{'uf':uf},context_instance=RequestContext(req))
